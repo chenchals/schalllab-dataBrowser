10cf635870865de1f920166e65a55bd682842a77
Chenchal Subraveti <chenchals@gmail.com>
2016-12-12 14:54:13 -0600

    initial SQL

---
+++ db/schalllab_schema.sql
@@ -0,0 +1,44 @@
+
+-- Schema for schalllab metadata
+CREATE TABLE study_subject(
+  subject_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  subject_species VARCHAR(100),
+  subject_name VARCHAR(100),
+  subject_gender VARCHAR(1),
+  subject_dob DATE
+);
+CREATE TABLE study(
+  study_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  subject_id INT(10) UNSIGNED NOT NULL,
+  person_id INT(10) UNSIGNED NOT NULL,
+  study_datafile VARCHAR(100),
+  study_description VARCHAR(100),
+  study_date DATE
+);
+CREATE TABLE person(
+  person_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  person_firstname VARCHAR(100),
+  person_lastname VARCHAR(100),
+  person_email VARCHAR(100)
+);
+CREATE TABLE person_study_role(
+  person_study_role_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  person_id INT(10) UNSIGNED NOT NULL,
+  study_id INT(10) UNSIGNED NOT NULL,
+  study_role VARCHAR(100)
+);
+
+
+
+
+
+-- Constraints
+ALTER TABLE
+  study ADD CONSTRAINT study_subject_fk FOREIGN KEY(subject_id) REFERENCES study_subject(subject_id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE
+  study ADD CONSTRAINT study_person_fk FOREIGN KEY(person_id) REFERENCES person(person_id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE
+  person_study_role ADD CONSTRAINT person_study_role_person_fk FOREIGN KEY(person_id, study_id) REFERENCES study(person_id, study_id) ON DELETE CASCADE ON UPDATE RESTRICT;
+  
+  
+  
